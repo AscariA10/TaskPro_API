c221321c12c448de3ffb5f971d25c878828b49bd
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-06-23 00:43:35 +0300

    add files for routing and update app for routing

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
-const contactsRouter = require('./routes/api/auth');
+const authRouter = require('./routes/api/auth');
+const dashboardRouter = require('./routes/api/dashboards');
+// const contactsRouter = require('./routes/api/auth');
 
 const app = express();
 
@@ -13,7 +15,9 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+app.use('/api/user', authRouter);
+app.use('/api/dashboard', dashboardRouter);
+// app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found on 3000' });
