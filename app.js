e4366052a419ea5b3fd24ac3d27533536c412bea
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-06-25 19:44:44 +0300

    add nodemailer

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 require("dotenv").config();
 
 const authRouter = require("./routes/api/auth");
@@ -15,6 +19,8 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/users", authRouter);
 
